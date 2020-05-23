20dd0e51cbc0763e50a943efcd99d94f24e0fe97
lssuseendharlal <lssuseendharlal@gmail.com>
2020-05-23 15:14:40 +0530

    lint

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 import data from "../data/data.json";
 class RestaurantCard extends React.Component {
   constructor() {
@@ -23,7 +23,11 @@ class RestaurantCard extends React.Component {
 
   render() {
     return (
-      <div className="cart" onClick={this.navigateOrder}>
+      <div
+        className="cart"
+        onClick={this.navigateOrder}
+        title={this.props.name}
+      >
         <center>
           <img src={this.props.thumbnail_image} alt={this.props.name} />
         </center>
